f8ddc232c9fcb580183df246758c71c79e9b4762
Patrick Martin <prof.pmartin10@gmail.com>
2015-01-29 20:58:00 -0500

    Initial commit with update project file and core files.

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to kalman-filter
+
+TODO: write [great documentation](http://jacobian.org/writing/great-documentation/what-to-write/)
---
+++ project.clj
@@ -0,0 +1,8 @@
+(defproject kalman-filter "0.1.0-SNAPSHOT"
+  :description "FIXME: write description"
+  :url "http://example.com/FIXME"
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :dependencies [[org.clojure/clojure "1.5.1"]
+                 [net.mikera/core.matrix "0.32.1"]
+                 [incanter "1.5.6"]])
